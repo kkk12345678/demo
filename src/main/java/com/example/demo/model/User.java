1761e31822d7a520f299e71910a443688626f55a
kkk12345678 <kkk12345678@users.noreply.github.com>
2022-05-26 17:58:22 +0300

    Merged to main

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+}
+
